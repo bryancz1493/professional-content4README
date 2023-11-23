40f7ea6fd649cddd03e4475249c710ae09b5a659
bryancz1493 <bryancz1493@gmail.com>
2023-11-22 19:14:59 -0800

    JS: Added some questions and test in terminal

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,40 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'GitHub Username',
+        message: 'Enter your GitHub username'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project name',
+    },
+    {
+        type: 'list',
+        name: 'License Type',
+        choices: [inquirer.Separator(),'MIT License', new inquirer.Separator(), 'Morzilla Public License 2.0'],
+    },
+];
+
+// Add README template
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((response) => {
+    writeToFile('README-challenge.md', response.title);
+    })
+}
 
 // Function call to initialize app
 init();
